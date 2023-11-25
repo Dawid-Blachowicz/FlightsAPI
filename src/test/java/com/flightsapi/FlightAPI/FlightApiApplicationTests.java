916bd2a0b338e718fcd768a35781d6f72336e1c9
Dawid-Blachowicz <david.blachowicz@gmail.com>
2023-11-25 11:01:06 +0100

    Initial commit

---
+++ src/test/java/com/flightsapi/FlightAPI/FlightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightsapi.FlightAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
